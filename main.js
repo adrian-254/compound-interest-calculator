3e8fc4ca9458b97f8a5fa53246b13cd3f30d8c16
adrian-254 <148047071+adrian-254@users.noreply.github.com>
2024-07-26 13:27:10 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+const Calculate = () => {
+    // Constants
+    const principal = Number(document.getElementById("principalLabel").value);
+    const duration = Number(document.getElementById("durationLabel").value);
+    const rate = Number(document.getElementById("rateLabel").value);
+    const interest = document.getElementById("interestSpan");
+    const amount = document.getElementById("amountSpan");
+    const monthlyRadio = document.getElementById("monthlyRadio");
+    const annuallyRadio = document.getElementById("annuallyRadio");
+
+    // Calculate based on radio button selection
+    if (monthlyRadio.checked) {
+        // Monthly compounding
+        let Money = principal * Math.pow(1 + rate / 100, duration / 12);
+        amount.innerHTML = Money.toFixed(2);  // Display total amount with 2 decimal places
+        interest.innerHTML = (Money - principal).toFixed(2);  // Display interest with 2 decimal places
+    } else if (annuallyRadio.checked) {
+        // Annual compounding
+        let Money = principal * Math.pow(1 + rate / 100, duration);
+        amount.innerHTML = Money.toFixed(2);  // Display total amount with 2 decimal places
+        interest.innerHTML = (Money - principal).toFixed(2);  // Display interest with 2 decimal places
+    }
+};
