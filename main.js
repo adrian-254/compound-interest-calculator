253154408f376c40a9990aea71726cc7aeec34c1
adrian-254 <148047071+adrian-254@users.noreply.github.com>
2025-03-31 09:58:56 +0300

    Update main.js

--- main.js
+++ main.js
@@ -7,17 +7,18 @@ const Calculate = () => {
     const amount = document.getElementById("amountSpan");
     const monthlyRadio = document.getElementById("monthlyRadio");
     const annuallyRadio = document.getElementById("annuallyRadio");
+    const tarehe = date.getFullYear();
 
-    // Calculate based on radio button selection
+    //the calculation
     if (monthlyRadio.checked) {
         // Monthly compounding
         let Money = principal * Math.pow(1 + rate / 100, duration / 12);
-        amount.innerHTML = Money.toFixed(2);  // Display total amount with 2 decimal places
-        interest.innerHTML = (Money - principal).toFixed(2);  // Display interest with 2 decimal places
+        amount.innerHTML = Money.toFixed(2);  
+        interest.innerHTML = (Money - principal).toFixed(2);  // Display interest with 2 d.place
     } else if (annuallyRadio.checked) {
         // Annual compounding
         let Money = principal * Math.pow(1 + rate / 100, duration);
         amount.innerHTML = Money.toFixed(2);  // Display total amount with 2 decimal places
-        interest.innerHTML = (Money - principal).toFixed(2);  // Display interest with 2 decimal places
+        interest.innerHTML = (Money - principal).toFixed(2);  // Display interest with 2.places
     }
 };
